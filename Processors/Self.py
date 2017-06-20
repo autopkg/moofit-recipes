e1df6d49e1d1044b6f5a67910be4d2cdf1008df0
Stephen Warneford-Bygrave <sbygrave@wegomac.local>
2017-06-20 22:57:07 +0100

    Renamed "Rsync" to "Processors" to contain all Shared processors

---
+++ Processors/Self.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#
+# Copyright 2015 Nick McSpadden
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from autopkglib import Processor, ProcessorError
+
+import subprocess
+import os.path
+
+__all__ = ["Self"]
+
+class Self(Processor):
+    description = "Provides an output of self.env."
+    input_variables = {
+    }
+    output_variables = {
+    }
+
+    __doc__ = description
+
+    def main(self):
+            self.output(self.env)
+
+if __name__ == "__main__":
+    processor = Self()
+    processor.execute_shell()

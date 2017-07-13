141779634621bd8fc699a325d12898d8e68b28b4
wegotoeleven <sbygrave@mac.com>
2017-07-13 19:53:28 +0100

    Added script remover and amended PkgCreator permissions

--- Xcode/additional/componentsInstaller.sh
+++ Xcode/additional/componentsInstaller.sh
@@ -5,7 +5,8 @@ installer -pkg /Applications/Xcode.app/Contents/Resources/Packages/MobileDevice.
 installer -pkg /Applications/Xcode.app/Contents/Resources/Packages/MobileDeviceDevelopment.pkg -target / -verbose
 installer -pkg /Applications/Xcode.app/Contents/Resources/Packages/XcodeSystemResources.pkg -target / -verbose
 
-# Stop and remove LaunchDaemon
+# Stop and remove LaunchDaemon and script
 launchctl stop uk.co.amsys.Xcode.components.plist
 launchctl remove uk.co.amsys.Xcode.components.plist
 rm /tmp/uk.co.amsys.Xcode.components.plist
+rm /tmp/componentsInstaller.sh

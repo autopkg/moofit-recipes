d394b6a6f3745b947142e5bee2b9ebde9651e100
Stephen Warneford-Bygrave <sbygrave@mac.com>
2020-08-20 13:24:35 +0100

    New Autodesk recipes

---
+++ Autodesk/AutodeskPostInstallSetup.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+# Author:   Stephen Bygrave - Moof IT
+# Name:     AutodeskPostInstallSetup.sh
+#
+# Purpose:  This script is used to add the version variable into the AutoDesk postinstall script
+#
+# Version:  1.0.0, 2020-08-07
+#           SB - Initial Creation
+
+# Use at your own risk. Moof IT will accept no responsibility for loss or damage caused by this script.
+
+# Imports
+
+from __future__ import absolute_import, print_function
+import os
+from autopkglib import Processor, ProcessorError, get_pref
+
+# Run script
+
+__all__ = ["AutodeskPostInstallSetup"]
+
+
+class AutodeskPostInstallSetup(Processor):
+    description = "Adds supplied keys to an AutoDesk app's postinstall \
+                    script that will be included within the generated pkg."
+    input_variables = {
+        "NAME": {
+            "description": "AutoDesk application name.",
+            "required": True
+        },
+        "PRODUCT_KEY": {
+            "description": "AutoDesk application product key.",
+            "required": True
+        },
+        "VERSION": {
+            "description": "AutoDesk application version number (i.e. 2021).",
+            "required": True
+        },
+        "EULA_LOCALE": {
+            "description": "AutoDesk application EULA locale.",
+            "required": True
+        },
+        "SERIAL_NUMBER": {
+            "description": "AutoDesk application serial number.",
+            "required": True
+        },
+        "LIC_SERVER": {
+            "description": "AutoDesk application licensing server. Currently only compatible with one server.",
+            "required": False
+        }
+    }
+    output_variables = {
+    }
+
+    def main(self):
+
+        # Open the postinstall script, substitute the registration info, write changes.
+        name = self.env['NAME']
+        prod_key = self.env['PRODUCT_KEY']
+        version = self.env['VERSION']
+        prod_ver = self.env['PRODUCT_VERSION']
+        eula_locale = self.env['EULA_LOCALE']
+        serial_number = self.env['SERIAL_NUMBER']
+        lic_servers = self.env['LIC_SERVER']
+        path = os.path.join(os.path.dirname(__file__),
+                            'scripts/AutodeskPostInstallTmp')
+        print(path)
+        if name:
+            filetext = None
+            with open(path, 'r') as file:
+                filetext = file.read()
+            if '%NAME%' in filetext:
+                filetext = filetext.replace('%NAME%', "%s" % (name))
+                with open(path, 'w') as file:
+                    file.write(filetext)
+
+        if prod_key:
+            filetext = None
+            with open(path, 'r') as file:
+                filetext = file.read()
+            if '%PK%' in filetext:
+                filetext = filetext.replace('%PK%', "%s" % (prod_key))
+                with open(path, 'w') as file:
+                    file.write(filetext)
+
+        if version:
+            filetext = None
+            with open(path, 'r') as file:
+                filetext = file.read()
+            if '%VER%' in filetext:
+                filetext = filetext.replace('%VER%', "%s" % (version))
+                with open(path, 'w') as file:
+                    file.write(filetext)
+
+        if prod_ver is not None:
+            filetext = None
+            with open(path, 'r') as file:
+                filetext = file.read()
+            if '%PV%' in filetext:
+                filetext = filetext.replace('%PV%', "%s" % (prod_ver))
+                with open(path, 'w') as file:
+                    file.write(filetext)
+
+        if eula_locale:
+            filetext = None
+            with open(path, 'r') as file:
+                filetext = file.read()
+            if '%EL%' in filetext:
+                filetext = filetext.replace('%EL%', "%s" % (eula_locale))
+                with open(path, 'w') as file:
+                    file.write(filetext)
+
+        if serial_number:
+            filetext = None
+            with open(path, 'r') as file:
+                filetext = file.read()
+            if '%SN%' in filetext:
+                filetext = filetext.replace('%SN%', "%s" % (serial_number))
+                with open(path, 'w') as file:
+                    file.write(filetext)
+
+        if lic_servers:
+            filetext = None
+            with open(path, 'r') as file:
+                filetext = file.read()
+            if '%LM%' in filetext:
+                filetext = filetext.replace('%LM%', "NETWORK")
+                with open(path, 'w') as file:
+                    file.write(filetext)
+            if '%LS%' in filetext:
+                filetext = filetext.replace('%LS%', "%s" % (lic_servers))
+                with open(path, 'w') as file:
+                    file.write(filetext)
+        else:
+            filetext = None
+            with open(path, 'r') as file:
+                filetext = file.read()
+            if '%LM%' in filetext:
+                filetext = filetext.replace('%LM%', "STANDALONE")
+                with open(path, 'w') as file:
+                    file.write(filetext)
+            if '%LS%' in filetext:
+                filetext = filetext.replace('--lic_servers \'@\'%LS%', "")
+                with open(path, 'w') as file:
+                    file.write(filetext)
+
+if __name__ == "__main__":
+    processor = AutodeskPostInstallSetup()
+    processor.execute_shell()
---
+++ Autodesk/README.md
@@ -0,0 +1,45 @@
+![Logo](https://x1llu7x4a4-flywheel.netdna-ssl.com/wp-content/themes/moof/images/logo.svg)
+
+# Autodesk Recipes
+
+## Table of Contents
+
+- [Table of Contents](#table-of-contents)
+- [Purpose](#purpose)
+- [Includes:](#includes)
+- [How to contribute](#how-to-contribute)
+- [Support](#support)
+- [Credit](#credit)
+- [License](#license)
+  
+## Purpose
+
+TBC
+
+## Includes:
+
+- Item 1
+- Item 2
+- Etc
+
+## How to contribute
+
+1. Fork this project, if required
+2. Create a new branch (`git checkout -b myNewBranch`)
+3. Make changes, and commit (`git commit -am "myChanges"`)
+4. Push to the new branch (`git push origin myNewBranch`)
+5. Create new pull request
+
+## Support
+
+Use at your own risk. Moof IT will accept no responsibility for loss or damage caused by these scripts. Contact Moof IT if you need a custom script tailored to your environment.
+
+## Credit
+
+Lorem Ipsum
+
+## License
+
+This work is licensed under http://creativecommons.org/licenses/by/4.0/.
+
+These scripts may be freely modified for personal or commercial purposes but may not be republished for profit without prior consent.
